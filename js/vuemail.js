692a8a57597906f51978612ae30fdbe25f16f52d
dieghino26 <brotzadiego@gmail.com>
2024-01-30 09:32:41 +0100

    created vue app

---
+++ js/vuemail.js
@@ -0,0 +1,13 @@
+// create Vue app
+const { createApp } = Vue;
+
+const endpoint = "https://flynn.boolean.careers/exercises/api/random/mail";
+
+const app = Vue.createApp({
+    data() {
+        return {
+
+        }
+    }
+});
+app.mount("#root");
