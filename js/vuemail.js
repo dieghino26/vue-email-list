74126a1105d7d6b12d5fd3450625e71b928a177e
dieghino26 <brotzadiego@gmail.com>
2024-01-30 09:50:52 +0100

    added 10 emails

--- js/vuemail.js
+++ js/vuemail.js
@@ -1,12 +1,19 @@
 // create Vue app
-const { createApp } = Vue;
-
+console.log("Vue ok", Vue)
 const endpoint = "https://flynn.boolean.careers/exercises/api/random/mail";
 
 const app = Vue.createApp({
     data() {
         return {
-
+            emails: []
+        }
+    },
+    created() {
+        for (let i = 1; i <= 10; i++) {
+            axios.get(endpoint).then((res) => {
+                const email = res.data.response
+                this.emails.push(email)
+            })
         }
     }
 });
